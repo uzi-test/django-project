36bd81cf1365e9ab6d6d21636cf076667ef0a21a
uzair aslam <uzairaslam92@yahoo.com>
2025-12-28 23:34:42 -1100

    Fix build.sh execution for Render

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "==> Installing requirements"
 pip install -r requirements.txt
-
-echo "==> Collecting static files"
 python manage.py collectstatic --noinput
-
-echo "==> Running migrations"
 python manage.py migrate
