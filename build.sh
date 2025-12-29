399d1587df7a2fe891655042e23d21354f6d63ca
uzair aslam <uzairaslam92@yahoo.com>
2025-12-28 14:53:29 -1100

    Set PYTHONPATH and DJANGO_SETTINGS_MODULE for Render

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 set -o errexit
 
 cd /opt/render/project/src
-
-python -c "import os,sys; print('PWD=',os.getcwd()); print('FILES=',os.listdir('.')); print('PY=',sys.version)"
-python -c "import pharmacy_project.settings; print('IMPORT OK')"
+export PYTHONPATH=/opt/render/project/src
+export DJANGO_SETTINGS_MODULE=pharmacy_project.settings
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
