b7ca2878dd6a71462058aef1d2988bc0b3e8cd5d
uzair aslam <uzairaslam92@yahoo.com>
2025-12-28 14:47:44 -1100

    Debug working dir and fix start command

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+cd /opt/render/project/src
+
+python -c "import os,sys; print('PWD=',os.getcwd()); print('FILES=',os.listdir('.')); print('PY=',sys.version)"
+python -c "import pharmacy_project.settings; print('IMPORT OK')"
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
